a9403c7d4a2e7570c3d0691ff4726426acc9a84b
jordan12sam <jordan12sam@gmail.com>
2026-05-06 20:38:53 +0100

    update terraform org

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,12 @@
 # Terraform Cloud backend
 # 1. Create account at https://app.terraform.io
-# 2. Create organization (e.g., "samuelmjordan")
+# 2. Create organization (e.g., "axolhost")
 # 3. Run: terraform login
 # 4. Run: terraform init
 
 terraform {
   cloud {
-    organization = "samuelmjordan"
+    organization = "axolhost"
 
     workspaces {
       name = "hosting-platform"
