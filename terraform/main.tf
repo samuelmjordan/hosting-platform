549e863c289166a25495531dbc72ea8822ec78a8
jordan12sam <jordan12sam@gmail.com>
2026-05-11 21:30:44 +0100

    cheaper box

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,7 +52,7 @@ resource "hcloud_firewall" "main" {
 resource "hcloud_server" "main" {
   name        = "hosting-platform"
   image       = "ubuntu-24.04"
-  server_type = "ccx13"
+  server_type = "cx33"
   location    = "fsn1"
 
   ssh_keys = [hcloud_ssh_key.main.id]
